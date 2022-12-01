cbaaf0ffbc42819456ad295ca319c311eee89728
Arianna Bunn <ajbunny12@gmail.com>
2022-12-01 15:23:17 -0500

    index.jsx view rendered at pokemon route

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const fs = require('fs')
 
 const pokemon = require('./models/pokemon.js');
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.get('/', (req, res) =>{
     res.send("Welcome to the Pokemon App!");
 })
 
 app.get('/pokemon/', (req, res) =>{
-    res.send(pokemon);
+    res.render("Index", {allPokemon: pokemon});
 })
 
 
---
+++ views/Index.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const myStyle = {
+    color: '#ffffff',
+    backgroundColor: '#000000',
+  };
+
+  class Index extends React.Component {
+    render () {
+    return (
+      <h1 style={myStyle}>WTF WTF WTF WTF WTF</h1>)
+    }
+  };
+
+  module.exports = Index;
